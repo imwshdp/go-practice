80239a54a5655416542c26206aafbe47a196c440
imwshdp <im.wshdp@gmail.com>
2025-08-12 01:24:57 +0300

    chore: docker commands moved

--- stepik/three/runner.sh
+++ stepik/three/runner.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-go run main.go testdata -f
+# for run with and without flags
+# go run main.go testdata -f
 # go run main.go testdata
+
+# docker container up
+# docker build -t golang_hw1_tree .
+# docker run -it --rm golang_hw1_tree
