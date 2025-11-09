224c373fd1e8c2186477b7da987bf2b537b28cd9
imwshdp <im.wshdp@gmail.com>
2025-11-09 16:13:58 +0300

    feature: migrations init

---
+++ backend-dev/http-based/cmd/migrate/migrations/000001_init_create.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users(
+  id bigserial PRIMARY KEY,
+  email varchar(255) UNIQUE NOT NULL,
+  username varchar(255) UNIQUE NOT NULL,
+  password bytea NOT NULL,
+  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+  id bigserial PRIMARY KEY,
+  title text NOT NULL,
+  user_id bigint NOT NULL,
+  content text NOT NULL,
+  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+);
