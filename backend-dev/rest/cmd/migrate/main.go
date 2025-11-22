297f96565fe059951fd9125f5e62de6aecf932c4
imwshdp <im.wshdp@gmail.com>
2025-11-22 19:29:59 +0300

    migrate: users table

---
+++ backend-dev/rest/cmd/migrate/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"rest/internal/config"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	log.Println("Starting migration...")
+
+	cfg := config.Load()
+
+	log.Println("cfg = ", cfg)
+
+	db, err := sql.Open("postgres", cfg.DbConfig.Addr)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Postgres connection establish error: %s", err))
+	}
+
+	defer func() {
+		db.Close()
+		log.Println("Database connection closed after migration")
+	}()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(fmt.Errorf("Database ping failed: %s", err))
+	}
+
+	log.Println("Database connection opened for migration")
+
+	m, err := migrate.New(
+		"file://cmd/migrate/migrations",
+		cfg.DbConfig.Addr,
+	)
+
+	if err != nil {
+		log.Fatal(fmt.Errorf("Migration init failed: %s", err))
+	}
+
+	cmd := os.Args[(len(os.Args) - 1)]
+
+	switch cmd {
+	case "up":
+		if err := m.Up(); err != nil {
+			log.Fatal("Migration up failed", err)
+		}
+
+	case "down":
+		if err := m.Down(); err != nil {
+			log.Fatal("Migration down failed", err)
+		}
+
+	default:
+		log.Fatal("Unknown command")
+	}
+
+	log.Println("Migration completed successfully")
+}
---
+++ backend-dev/rest/cmd/migrate/migrations/20251122154619_add-user-table.up.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = NOW();
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_users_updated_at
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at_column();
